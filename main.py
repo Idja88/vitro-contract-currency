385c1d1132fa534410883bc2227c1cb3a2836f4e
Idja88 <idja08@gmail.com>
2024-07-05 12:15:53 +0500

    BeginDate and MailException addins

--- main.py
+++ main.py
@@ -5,18 +5,39 @@ from datetime import datetime, timedelta
 import pandas as pd
 import requests
 import sqlalchemy as sa
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 def connect_to_db(connection_string):
     connection_uri = f"mssql+pyodbc:///?odbc_connect={urllib.parse.quote_plus(connection_string)}"
-    engine = sa.create_engine(connection_uri, fast_executemany=True, echo=True)
+    engine = sa.create_engine(connection_uri, fast_executemany=True, echo=False)
     return engine
 
-def main(rates, currencies, connection_string):
-    end_date = datetime.now()
-    begin_date = end_date - timedelta(days=0)
-    end_date_str = end_date.strftime('%d.%m.%Y')
-    begin_date_str = begin_date.strftime('%d.%m.%Y')
+def send_email(subject, message, from_email, to_emails, smtp_server, smtp_port):
+    try:
+        server = smtplib.SMTP(smtp_server, smtp_port)
+        for to_email in to_emails:
+            msg = MIMEMultipart()
+            msg['From'] = from_email
+            msg['To'] = to_email
+            msg['Subject'] = subject
+            msg.attach(MIMEText(message, 'plain'))
+            server.send_message(msg)
+    except Exception as e:
+        print("Error sending email:", str(e))
+    finally:
+        server.quit()
 
+def main(rates, currencies, connection_string, begin_date):
+    end_date = datetime.now()
+    end_date_str = end_date.strftime('%d.%m.%Y')
+
+    if begin_date == "":
+        begin_date = end_date - timedelta(days=0)
+        begin_date_str = begin_date.strftime('%d.%m.%Y')
+    else: begin_date_str = begin_date
+    
     url = 'https://nationalbank.kz/ru/exchangerates/ezhednevnye-oficialnye-rynochnye-kursy-valyut/excel'
     params = {
         'beginDate': begin_date_str,
@@ -47,7 +68,7 @@ def main(rates, currencies, connection_string):
         except Exception as e:
             print(f"Failed to insert data into the database: {e}")
         finally:
-            engine.dispose()            
+            engine.dispose()          
     else:
         print(f'Failed to download file. Status code: {response.status_code}')
 
@@ -55,7 +76,18 @@ if __name__ == "__main__":
     config_path = os.path.join(os.getcwd(), 'config.json')
     with open(config_path, "r", encoding="utf-8") as config_file:
         config = json.load(config_file)
-        connection_string = config['connection_string']
+        begin_date = config['begin_date']
         currencies = list(config['currency_code'].keys())
         rates = list(config['currency_code'].values())
-    main(rates=rates, currencies=currencies, connection_string=connection_string)
+        connection_string = config['connection_string']
+        from_email = config['mail_message']['from_email']
+        to_emails = config['mail_message']['to_emails']
+        smtp_server = config['mail_message']['smtp_server']
+        smtp_port = config['mail_message']['smtp_port']
+
+    try:
+        main(rates=rates, currencies=currencies, connection_string=connection_string, begin_date=begin_date)
+    except Exception as e:
+        error_message = f"An error occurred: {str(e)}"
+        send_email("Error in ETL process", error_message, from_email, to_emails, smtp_server, smtp_port)
+        raise SystemExit(1)
