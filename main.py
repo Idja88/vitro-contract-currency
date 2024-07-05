5e4d9be368bd8331e79b8d68fb1615b037d6e91c
Idja88 <idja08@gmail.com>
2024-07-05 16:23:55 +0500

    Division into few functions

--- main.py
+++ main.py
@@ -9,10 +9,55 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
+def get_exchange_rates(rates, begin_date):
+    end_date = datetime.now()
+    end_date_str = end_date.strftime('%d.%m.%Y')
+    if begin_date == "":
+        begin_date = end_date - timedelta(days=0)
+        begin_date_str = begin_date.strftime('%d.%m.%Y')
+    else: 
+        begin_date_str = begin_date
+    url = 'https://nationalbank.kz/ru/exchangerates/ezhednevnye-oficialnye-rynochnye-kursy-valyut/excel'
+    params = {
+        'beginDate': begin_date_str,
+        'endDate': end_date_str,
+        'rates[]': rates
+    }
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        filename = 'exchange_rates.xlsx'
+        file_path = f'C:\\Windows\\Temp\\{filename}'
+        with open(file_path, 'wb') as file:
+            file.write(response.content)
+        print(f'File saved as {file_path}')
+    return file_path
+
+def transform_data(file_path, currencies):
+    df = pd.read_excel(file_path, engine='openpyxl')
+    for currency in currencies:
+        quant_column = f"{currency}_quant"
+        df[currency] = df[currency] / df[quant_column]
+    df_melted = df.melt(id_vars=['Date'], value_vars=currencies, var_name='CurrencyCode', value_name='CurrencyValue')
+    df_melted.columns = ['DATE', 'CurrencyCode', 'CurrencyValue']
+    df_melted['DATE'] = pd.to_datetime(df_melted['DATE'], format='%d.%m.%Y', errors='coerce')
+    return df_melted
+
 def connect_to_db(connection_string):
     connection_uri = f"mssql+pyodbc:///?odbc_connect={urllib.parse.quote_plus(connection_string)}"
     engine = sa.create_engine(connection_uri, fast_executemany=True, echo=False)
     return engine
+
+def load_data(df, connection_string):
+    try:
+        engine = connect_to_db(connection_string)
+        with engine.connect() as connection:
+            print(df)
+            df.to_sql('currency_history', connection, if_exists='append', index=False)
+            print("Data has been inserted successfully.")
+    except Exception as e:
+            print(f"Failed to insert data into the database: {e}")
+    finally:
+        engine.dispose()      
 
 def send_email(subject, message, from_email, to_emails, smtp_server, smtp_port):
     try:
@@ -30,50 +75,9 @@ def send_email(subject, message, from_email, to_emails, smtp_server, smtp_port):
         server.quit()
 
 def main(rates, currencies, connection_string, begin_date):
-    end_date = datetime.now()
-    end_date_str = end_date.strftime('%d.%m.%Y')
-
-    if begin_date == "":
-        begin_date = end_date - timedelta(days=0)
-        begin_date_str = begin_date.strftime('%d.%m.%Y')
-    else: begin_date_str = begin_date
-    
-    url = 'https://nationalbank.kz/ru/exchangerates/ezhednevnye-oficialnye-rynochnye-kursy-valyut/excel'
-    params = {
-        'beginDate': begin_date_str,
-        'endDate': end_date_str,
-        'rates[]': rates
-    }
-
-    response = requests.get(url, params=params)
-
-    if response.status_code == 200:
-        filename = 'exchange_rates.xlsx'
-        file_path = f'C:\\Windows\\Temp\\{filename}'
-        
-        with open(file_path, 'wb') as file:
-            file.write(response.content)
-        print(f'File saved as {file_path}')
-
-        df = pd.read_excel(file_path, engine='openpyxl')
-        for currency in currencies:
-            quant_column = f"{currency}_quant"
-            df[currency] = df[currency] / df[quant_column]
-        df_melted = df.melt(id_vars=['Date'], value_vars=currencies, var_name='CurrencyCode', value_name='CurrencyValue')
-        df_melted.columns = ['DATE', 'CurrencyCode', 'CurrencyValue']
-        df_melted['DATE'] = pd.to_datetime(df_melted['DATE'], format='%d.%m.%Y', errors='coerce')
-
-        try:
-            engine = connect_to_db(connection_string)
-            with engine.connect() as connection:
-                df_melted.to_sql('currency_history', connection, if_exists='append', index=False)
-                print("Data has been inserted successfully.")
-        except Exception as e:
-            print(f"Failed to insert data into the database: {e}")
-        finally:
-            engine.dispose()          
-    else:
-        print(f'Failed to download file. Status code: {response.status_code}')
+    file_path = get_exchange_rates(rates=rates, begin_date=begin_date)
+    transformed_df = transform_data(file_path=file_path, currencies=currencies)
+    load_data(transformed_df, connection_string=connection_string)
 
 if __name__ == "__main__":
     config_path = os.path.join(os.getcwd(), 'config.json')
