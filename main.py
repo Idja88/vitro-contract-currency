7c873c7d84800d2d77962bf371e4916971291d96
Idja88 <idja08@gmail.com>
2026-04-23 16:04:41 +0500

    Added new config parameters of table_name and table_schema. Dataframe column name corrected to column name in database

--- main.py
+++ main.py
@@ -15,14 +15,14 @@ class DateValidationError(Exception):
 class DateLogicError(Exception):
     pass
 
-def validate_date(date_string):
+def validate_date(date_string: str) -> bool:
     try:
         datetime.strptime(date_string, '%d.%m.%Y')
         return True
     except ValueError:
          raise DateValidationError(f"Invalid date format: {date_string}")
 
-def get_exchange_rates(url, temp_path, rates, begin_date, end_date):
+def get_exchange_rates(url: str, temp_path: str, rates: list, begin_date: str, end_date: str) -> str:
     if end_date == "":
         end_date = datetime.now()
         end_date_str = end_date.strftime('%d.%m.%Y')
@@ -50,22 +50,21 @@ def get_exchange_rates(url, temp_path, rates, begin_date, end_date):
         file_path = os.path.join(temp_path, filename)
         with open(file_path, 'wb') as file:
             file.write(response.content)
-        print(f'File saved as {file_path}')
         return file_path
     else:
         raise requests.exceptions.HTTPError(f"Failed to download file. Status code: {response.status_code}")
 
-def transform_data(file_path, currencies):
+def transform_data(file_path: str, currencies: list) -> pd.DataFrame:
     df = pd.read_excel(file_path, engine='openpyxl')
     for currency in currencies:
         quant_column = f"{currency}_quant"
         df[currency] = df[currency] / df[quant_column]
-    df_melted = df.melt(id_vars=['Date'], value_vars=currencies, var_name='CurrencyCode', value_name='CurrencyValue')
-    df_melted.columns = ['DATE', 'CurrencyCode', 'CurrencyValue']
-    df_melted['DATE'] = pd.to_datetime(df_melted['DATE'], format='%d.%m.%Y', errors='coerce')
+    df_melted = df.melt(id_vars=['Date'], value_vars=currencies, var_name='currency_code', value_name='currency_value')
+    df_melted.columns = ['date', 'currency_code', 'currency_value']
+    df_melted['date'] = pd.to_datetime(df_melted['date'], format='%d.%m.%Y', errors='coerce')
     return df_melted
 
-def connect_to_db(connection_string: str):
+def connect_to_db(connection_string: str) -> sa.engine.base.Engine:
     parts = {}
     for param in connection_string.split(';'):
         if '=' in param:
@@ -96,16 +95,16 @@ def connect_to_db(connection_string: str):
     
     return engine
 
-def load_data(df, connection_string):
+def load_data(df: pd.DataFrame, connection_string: str, table_name: str, table_schema: str):
     try:
         engine = connect_to_db(connection_string)
         with engine.connect() as connection:
-            df.to_sql('currency_history', connection, if_exists='append', index=False)
+            df.to_sql(name=table_name, con=connection, schema=table_schema, if_exists='append', index=False)
             print("Data has been inserted successfully.")
     finally:
         engine.dispose()
 
-def send_email(subject, message, from_email, to_emails, smtp_server, smtp_port, smtp_login, smtp_password):
+def send_email(subject: str, message: str, from_email: str, to_emails: list, smtp_server: str, smtp_port: int, smtp_login: str, smtp_password: str):
     try:
         server = smtplib.SMTP(smtp_server, smtp_port)
         server.starttls()
@@ -122,17 +121,19 @@ def send_email(subject, message, from_email, to_emails, smtp_server, smtp_port, 
     finally:
         server.quit()
 
-def main(url, temp_path,rates, currencies, connection_string, begin_date, end_date):
+def main(url: str, temp_path: str, table_name: str, table_schema: str, rates: list, currencies: list, connection_string: str, begin_date: str, end_date: str):
     file_path = get_exchange_rates(url=url, temp_path=temp_path, rates=rates, begin_date=begin_date, end_date=end_date)
     transformed_df = transform_data(file_path=file_path, currencies=currencies)
-    load_data(transformed_df, connection_string=connection_string)
+    load_data(df=transformed_df, connection_string=connection_string, table_name=table_name, table_schema=table_schema)
 
 if __name__ == "__main__":
     config_path = os.path.join(os.getcwd(), 'config.json')
     with open(config_path, "r", encoding="utf-8") as config_file:
         config = json.load(config_file)
-        temp_path = config['temp_path']
         url = config['url']
+        temp_path = config['temp_path']
+        table_name = config['table_name'] if config['table_name'] else "currency_history"
+        table_schema = config['table_schema'] if config['table_schema'] else None
         begin_date = config['begin_date']
         end_date = config['end_date']
         currencies = list(config['currency_code'].keys())
@@ -146,7 +147,17 @@ if __name__ == "__main__":
         smtp_password = config['mail_message']['smtp_password']
 
     try:
-        main(url=url, temp_path=temp_path, rates=rates, currencies=currencies, connection_string=connection_string, begin_date=begin_date, end_date=end_date)
+        main(
+            url=url,
+            temp_path=temp_path,
+            table_name=table_name,
+            table_schema=table_schema,
+            rates=rates,
+            currencies=currencies,
+            connection_string=connection_string,
+            begin_date=begin_date,
+            end_date=end_date
+            )
     except Exception as e:
         error_message = f"An error occurred: {str(e)}"
         send_email("Error in ETL process", error_message, from_email, to_emails, smtp_server, smtp_port, smtp_login, smtp_password)
